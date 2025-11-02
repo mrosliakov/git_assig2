c2377250093245bf88510dce0386abb7c2065999
mrosliakov <mrosliakov@kse.org.ua>
2025-11-02 19:40:50 +0200

    Adding submodules during creation was implemented.

--- repo-lifecycle.sh
+++ repo-lifecycle.sh
@@ -6,7 +6,9 @@ if [ -z "$repo_path" ]; then
   repo_path="." 
 fi
 
-repo_path="../testing/$repo_path" # todo: remove this part after testing
+repo_path="./testing/$repo_path" # todo: remove this part after testing
+cd "$repo_path" || exit 1
+
 
 create_repo() {
     # Asking for repository name
@@ -16,9 +18,8 @@ create_repo() {
     
     # Checking if the repository already exists
     
-    #if is_repo "$repo_name"; then
-    if is_repo "$new_repo_path"; then
-      echo "Repository already exists at $repo_path"
+    if is_repo "$repo_name"; then
+      echo "Repository already exists at $new_repo_path"
       return
     fi
 
@@ -35,12 +36,33 @@ create_repo() {
       return
     fi
     # Creating the repository
-    git init -b "$main_branch" "$repo_path"
-    cd "$repo_path" || exit 1
-    git commit --allow-empty -m "Initial commit"
+    git init -b "$main_branch" "$repo_name"
+    cd "$repo_name" || exit 1
+    git commit --allow-empty -m "Initial empty snapshot for $main_branch branch"
     echo "Initialized empty Git repository in $new_repo_path"
     
-    
+    # echo "Current directory after repo creation:"
+    # pwd
+
+    # Asking for submodules
+    read -p "How many submodules would you like to add (up to 3)? (0 for none): " submodule_count
+    if ! [[ "$submodule_count" =~ ^[0-3]$ ]]; then
+      echo "Invalid number of submodules. Skipping submodule addition."
+      return
+    fi
+
+    # Adding submodules
+    git config --global protocol.file.allow always
+    for (( i=1; i<=submodule_count; i++ )); do
+      read -p "Enter local path to add submodule $i: " submodule_path
+      read -p "Enter source directory of the submodule $i (relative to repository root): " submodule_source
+      source_abs_path=$(readlink -f "$submodule_source")
+      git submodule add "file://$source_abs_path" "$submodule_path"
+      echo "Added submodule $i: $submodule_source at $submodule_path"
+    done
+    git config --global --unset protocol.file.allow
+
+
 }
 
 validate_repo() {
@@ -51,14 +73,15 @@ submodule_repo() {
   echo "Submodule management implementation goes here."
 }
 
+# shellcheck disable=SC2120
 is_repo() {
   if [ -z "$1" ]; then
-    path_to_check="$repo_path"
+    path_to_check="."
   else 
     path_to_check="$1"
   fi
 
-  if [ -d "$path_to_check/.git" ]; then
+  if [ -d "./$path_to_check/.git" ]; then
     return 0
   else
     return 1
